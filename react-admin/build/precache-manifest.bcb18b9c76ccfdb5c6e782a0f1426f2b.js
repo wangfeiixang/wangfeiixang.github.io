f9d21ae57b7f1edd9bdfff4d5948e1bd626c61f4
wangfeixiang <wangfeixiang@testin.cn>
2019-05-09 16:12:48 +0800

    添加react数据管理系统

---
+++ react-admin/build/precache-manifest.bcb18b9c76ccfdb5c6e782a0f1426f2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "807e6215cb0942c4955d",
+    "url": "./static/js/main.64f141f9.chunk.js"
+  },
+  {
+    "revision": "4b9878a8a2c375b1d1b9",
+    "url": "./static/js/2.32674bbc.chunk.js"
+  },
+  {
+    "revision": "807e6215cb0942c4955d",
+    "url": "./static/css/main.a335b722.chunk.css"
+  },
+  {
+    "revision": "4b9878a8a2c375b1d1b9",
+    "url": "./static/css/2.f5f23763.chunk.css"
+  },
+  {
+    "revision": "dfb80e61514e689345fff40cd88cab40",
+    "url": "./index.html"
+  }
+];
